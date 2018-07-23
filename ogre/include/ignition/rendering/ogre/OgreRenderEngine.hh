abbe78fdaeef2ebcb3c6a298fdbcf0aa871c265c
Ian Chen <ichen@osrfoundation.org>
2018-07-23 14:36:07 -0700

    create render window function renaming, style fix

--- ogre/include/ignition/rendering/ogre/OgreRenderEngine.hh
+++ ogre/include/ignition/rendering/ogre/OgreRenderEngine.hh
@@ -99,9 +99,9 @@ namespace ignition
 
       public: virtual Ogre::Root *OgreRoot() const;
 
-      public: std::string CreateNewWindow(const std::string &_handle,
+      public: std::string CreateRenderWindow(const std::string &_handle,
                   const unsigned int _width, const unsigned int _height,
-                  const double __ratio, const unsigned int antiAliasing);
+                  const double _ratio, const unsigned int _antiAliasing);
 
       protected: virtual ScenePtr CreateSceneImpl(unsigned int _id,
                   const std::string &_name);
@@ -128,7 +128,7 @@ namespace ignition
 
       private: void CreateResources();
 
-      private: void CreateNewWindow();
+      private: void CreateRenderWindow();
 
       private: void CheckCapabilities();
 

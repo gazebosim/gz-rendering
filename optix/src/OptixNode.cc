9657447ef9c12c3886206a979d878b067b13fc18
d_hood <dhood@users.noreply.github.com>
2018-08-23 11:13:41 -0700

    Additional include for OptixStorage

--- optix/src/OptixNode.cc
+++ optix/src/OptixNode.cc
@@ -18,6 +18,7 @@
 #include "ignition/rendering/optix/OptixConversions.hh"
 #include "ignition/rendering/optix/OptixIncludes.hh"
 #include "ignition/rendering/optix/OptixScene.hh"
+#include "ignition/rendering/optix/OptixStorage.hh"
 
 using namespace ignition;
 using namespace rendering;

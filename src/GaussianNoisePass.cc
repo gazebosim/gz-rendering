dced5c13d57fbcbbb0e9a30ce55b2a4b5e0fa032
Jose Luis Rivero <jrivero@osrfoundation.org>
2019-04-08 22:40:59 +0000

    Provide default constructor/destructor

---
+++ src/GaussianNoisePass.cc
@@ -0,0 +1,12 @@
+#include "ignition/rendering/GaussianNoisePass.hh"
+
+using namespace ignition;
+using namespace rendering;
+
+GaussianNoisePass::GaussianNoisePass()
+{
+}
+
+GaussianNoisePass::~GaussianNoisePass()
+{
+}

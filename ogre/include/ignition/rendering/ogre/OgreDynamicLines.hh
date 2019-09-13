917fe0c59625d008e39e47170c6a4c441c480d61
jshep1 <devnull@localhost>
2019-09-13 16:42:18 -0700

    Add dynamic lines and renderable to ogre

---
+++ ogre/include/ignition/rendering/ogre/OgreDynamicLines.hh
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2012 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef IGNITION_RENDERING_OGRE_OGREDYNAMICLINES_HH_
+#define IGNITION_RENDERING_OGRE_OGREDYNAMICLINES_HH_
+
+#include <memory>
+#include <vector>
+#include <string>
+#include <ignition/math/Color.hh>
+
+#include "ignition/rendering/ogre/OgreConversions.hh"
+#include "ignition/rendering/ogre/Export.hh"
+#include "ignition/rendering/ogre/OgreDynamicRenderable.hh"
+
+namespace ignition
+{
+  namespace rendering
+  {
+    inline namespace IGNITION_RENDERING_VERSION_NAMESPACE {
+    /// \addtogroup gazebo_rendering
+    /// \{
+
+    /// \brief Forward declaration
+    class DynamicLinesPrivate;
+
+    /// \class DynamicLines DynamicLines.hh rendering/rendering.hh
+    /// \brief Class for drawing lines that can change
+    class IGNITION_RENDERING_OGRE_VISIBLE OgreDynamicLines : public OgreDynamicRenderable
+    {
+      /// \brief Constructor
+      /// \param[in] _opType The type of Line
+      public: explicit OgreDynamicLines(
+                  RenderOpType _opType = RENDERING_LINE_STRIP);
+
+      /// \brief Destructor
+      public: virtual ~OgreDynamicLines();
+
+      /// \brief Get type of movable
+      /// \return This returns "gazebo::dynamiclines"
+      public: static std::string GetMovableType();
+
+      /// \brief Overridden function from Ogre's base class.
+      /// \return Returns "gazebo::ogredynamicslines"
+      public: virtual const Ogre::String &getMovableType() const;
+
+      /// \brief Add a point to the point list
+      /// \param[in] _pt ignition::math::Vector3d point
+      /// \param[in] _color ignition::math::Color Point color
+      public: void AddPoint(const ignition::math::Vector3d &_pt,
+            const ignition::math::Color &_color = ignition::math::Color::White);
+
+      /// \brief Add a point to the point list.
+      /// \param[in] _x X position
+      /// \param[in] _y Y position
+      /// \param[in] _z Z position
+      /// \param[in] _color ignition::math::Color Point color
+      public: void AddPoint(const double _x, const double _y, const double _z,
+            const ignition::math::Color &_color = ignition::math::Color::White);
+
+      /// \brief Change the location of an existing point in the point list
+      /// \param[in] _index Index of the point to set
+      /// \param[in] _value ignition::math::Vector3d value to set the point to
+      public: void SetPoint(const unsigned int _index,
+                  const ignition::math::Vector3d &_value);
+
+      /// \brief Change the color of an existing point in the point list
+      /// \param[in] _index Index of the point to set
+      /// \param[in] _color ignition::math::Color Pixelcolor color to set the
+      /// point to
+      public: void SetColor(const unsigned int _index,
+                            const ignition::math::Color &_color);
+
+      /// \brief Return the location of an existing point in the point list
+      /// \param[in] _index Number of the point to return
+      /// \return ignition::math::Vector3d value of the point. A vector of
+      /// [ignition::math::INF_D, ignition::math::INF_D, ignition::math::INF_D]
+      /// is returned when then the _index is out of bounds.
+      /// ignition::math::INF_D==std::numeric_limits<double>::infinity()
+      public: ignition::math::Vector3d Point(const unsigned int _index) const;
+
+      /// \brief Return the total number of points in the point list
+      /// \return Number of points
+      public: unsigned int GetPointCount() const;
+
+      /// \brief Remove all points from the point list
+      public: void Clear();
+
+      /// \brief Call this to update the hardware buffer after making changes.
+      public: void Update();
+
+      /// \brief Implementation DynamicRenderable,
+      /// creates a simple vertex-only decl
+      private: virtual void  CreateVertexDeclaration();
+
+      /// \brief Implementation DynamicRenderable, pushes point
+      /// list out to hardware memory
+      private: virtual void FillHardwareBuffers();
+
+      /// \brief List of points for the line
+      private: std::vector<ignition::math::Vector3d> points;
+
+      /// \brief Used to indicate if the lines require an update
+      private: bool dirty;
+
+      /// \brief private implementation
+      private: std::unique_ptr<DynamicLinesPrivate> dataPtr;
+    };
+    }
+    /// \}
+  }
+}
+#endif
---
+++ ogre/src/OgreDynamicLines.cc
@@ -0,0 +1,204 @@
+/*
+ * Copyright (C) 2012 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#include <math.h>
+
+#include <cmath>
+#include <sstream>
+#include <ignition/math/Color.hh>
+//#include "gazebo/rendering/ogre_gazebo.h"
+
+#include "ignition/common/Console.hh"
+//#include "gazebo/common/Exception.hh"
+#include "ignition/rendering/ogre/OgreDynamicLines.hh"
+
+using namespace ignition;
+using namespace rendering;
+
+enum {POSITION_BINDING, TEXCOORD_BINDING};
+
+
+/// \brief Private implementation
+class ignition::rendering::DynamicLinesPrivate
+{
+  /// \brief list of colors at each point
+  public: std::vector<ignition::math::Color> colors;
+};
+
+/////////////////////////////////////////////////
+OgreDynamicLines::OgreDynamicLines(RenderOpType opType)
+  : dataPtr(new DynamicLinesPrivate)
+{
+  this->Init(opType, false);
+  this->setCastShadows(false);
+  this->dirty = true;
+}
+
+/////////////////////////////////////////////////
+OgreDynamicLines::~OgreDynamicLines()
+{
+}
+
+/////////////////////////////////////////////////
+std::string OgreDynamicLines::GetMovableType()
+{
+  return "gazebo::dynamiclines";
+}
+
+/////////////////////////////////////////////////
+const Ogre::String &OgreDynamicLines::getMovableType() const
+{
+  static Ogre::String moveType = OgreDynamicLines::GetMovableType();
+  return moveType;
+}
+
+/////////////////////////////////////////////////
+void OgreDynamicLines::AddPoint(const ignition::math::Vector3d &_pt,
+                            const ignition::math::Color &_color)
+{
+  this->points.push_back(_pt);
+  this->dataPtr->colors.push_back(_color);
+  this->dirty = true;
+}
+
+/////////////////////////////////////////////////
+void OgreDynamicLines::AddPoint(const double _x, const double _y, const double _z,
+                            const ignition::math::Color &_color)
+{
+  this->AddPoint(ignition::math::Vector3d(_x, _y, _z), _color);
+}
+
+/////////////////////////////////////////////////
+void OgreDynamicLines::SetPoint(const unsigned int _index,
+                            const ignition::math::Vector3d &_value)
+{
+  if (_index >= this->points.size())
+  {
+    ignerr << "Point index[" << _index << "] is out of bounds[0-"
+           << this->points.size()-1 << "]\n";
+    return;
+  }
+
+  this->points[_index] = _value;
+
+  this->dirty = true;
+}
+
+/////////////////////////////////////////////////
+void OgreDynamicLines::SetColor(const unsigned int _index,
+                            const ignition::math::Color &_color)
+{
+  this->dataPtr->colors[_index] = _color;
+  this->dirty = true;
+}
+
+/////////////////////////////////////////////////
+ignition::math::Vector3d OgreDynamicLines::Point(
+    const unsigned int _index) const
+{
+  if (_index >= this->points.size())
+  {
+    ignerr << "Point index[" << _index << "] is out of bounds[0-"
+           << this->points.size()-1 << "]\n";
+
+    return ignition::math::Vector3d(ignition::math::INF_D,
+                                    ignition::math::INF_D,
+                                    ignition::math::INF_D);
+  }
+
+  return this->points[_index];
+}
+
+/////////////////////////////////////////////////
+unsigned int OgreDynamicLines::GetPointCount() const
+{
+  return this->points.size();
+}
+
+/////////////////////////////////////////////////
+void OgreDynamicLines::Clear()
+{
+  this->points.clear();
+  this->dirty = true;
+}
+
+/////////////////////////////////////////////////
+void OgreDynamicLines::Update()
+{
+  if (this->dirty && this->points.size() > 1)
+    this->FillHardwareBuffers();
+}
+
+/////////////////////////////////////////////////
+void OgreDynamicLines::CreateVertexDeclaration()
+{
+  Ogre::VertexDeclaration *decl =
+    this->mRenderOp.vertexData->vertexDeclaration;
+
+  decl->addElement(POSITION_BINDING, 0, Ogre::VET_FLOAT3, Ogre::VES_POSITION);
+  decl->addElement(1, 0, Ogre::VET_COLOUR, Ogre::VES_DIFFUSE);
+}
+
+/////////////////////////////////////////////////
+void OgreDynamicLines::FillHardwareBuffers()
+{
+  int size = this->points.size();
+  this->PrepareHardwareBuffers(size, 0);
+
+  if (!size)
+  {
+    this->mBox.setExtents(Ogre::Vector3::ZERO, Ogre::Vector3::ZERO);
+    this->dirty = false;
+  }
+
+  Ogre::HardwareVertexBufferSharedPtr vbuf =
+    this->mRenderOp.vertexData->vertexBufferBinding->getBuffer(0);
+
+  Ogre::Real *prPos =
+    static_cast<Ogre::Real*>(vbuf->lock(Ogre::HardwareBuffer::HBL_NORMAL));
+  {
+    for (int i = 0; i < size; i++)
+    {
+      *prPos++ = this->points[i].X();
+      *prPos++ = this->points[i].Y();
+      *prPos++ = this->points[i].Z();
+
+      this->mBox.merge(OgreConversions::Convert(this->points[i]));
+    }
+  }
+  vbuf->unlock();
+
+  // Update the colors
+  Ogre::HardwareVertexBufferSharedPtr cbuf =
+    this->mRenderOp.vertexData->vertexBufferBinding->getBuffer(1);
+
+  Ogre::RGBA *colorArrayBuffer =
+        static_cast<Ogre::RGBA*>(cbuf->lock(Ogre::HardwareBuffer::HBL_DISCARD));
+  Ogre::RenderSystem *renderSystemForVertex =
+        Ogre::Root::getSingleton().getRenderSystem();
+  for (int i = 0; i < size; ++i)
+  {
+    Ogre::ColourValue color = OgreConversions::Convert(this->dataPtr->colors[i]);
+    renderSystemForVertex->convertColourValue(color, &colorArrayBuffer[i]);
+  }
+  cbuf->unlock();
+
+  // need to update after mBox change, otherwise the lines goes in and out
+  // of scope based on old mBox
+  this->getParentSceneNode()->needUpdate();
+
+  this->dirty = false;
+}

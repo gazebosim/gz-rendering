ba40422d1e648597b7e9f6ec608e3bf583b1aa0a
chapulina <burajiru.no.chapulina@gmail.com>
2019-12-20 12:08:36 -0800

    Skeletons for Ogre2

--- examples/actor_animation/Main.cc
+++ examples/actor_animation/Main.cc
@@ -90,15 +90,18 @@ void buildScene(ScenePtr _scene, MeshPtr &_mesh, common::SkeletonPtr &_skel)
   gray->SetSpecular(0.7, 0.7, 0.7);
 
   // create grid visual
-  VisualPtr grid = _scene->CreateVisual();
   GridPtr gridGeom = _scene->CreateGrid();
-  gridGeom->SetCellCount(20);
-  gridGeom->SetCellLength(1);
-  gridGeom->SetVerticalCellCount(0);
-  grid->AddGeometry(gridGeom);
-  grid->SetLocalPosition(3, 0, 0.0);
-  grid->SetMaterial(gray);
-  root->AddChild(grid);
+  if (gridGeom)
+  {
+    VisualPtr grid = _scene->CreateVisual();
+    gridGeom->SetCellCount(20);
+    gridGeom->SetCellLength(1);
+    gridGeom->SetVerticalCellCount(0);
+    grid->AddGeometry(gridGeom);
+    grid->SetLocalPosition(3, 0, 0.0);
+    grid->SetMaterial(gray);
+    root->AddChild(grid);
+  }
 
   // create camera
   CameraPtr camera = _scene->CreateCamera("camera");
@@ -137,17 +140,26 @@ int main(int _argc, char** _argv)
 {
   glutInit(&_argc, _argv);
 
+  // Expose engine name to command line because we can't instantiate both
+  // ogre and ogre2 at the same time
+  std::string engine("ogre");
+  if (_argc > 1)
+  {
+    engine = _argv[1];
+  }
+
   common::Console::SetVerbosity(4);
   std::vector<std::string> engineNames;
   std::vector<CameraPtr> cameras;
 
-  engineNames.push_back("ogre");
+  engineNames.push_back(engine);
 
   MeshPtr mesh = nullptr;
   ic::SkeletonPtr skel = nullptr;
 
   for (auto engineName : engineNames)
   {
+    std::cout << "Starting engine [" << engineName << "]" << std::endl;
     try
     {
       CameraPtr camera = createCamera(engineName, mesh, skel);

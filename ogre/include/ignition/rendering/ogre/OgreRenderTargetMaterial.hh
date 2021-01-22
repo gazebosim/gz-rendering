e794e8862fbc70affab27390911b4dadfa9f3b2f
Louise Poubel <louise@openrobotics.org>
2021-01-21 19:20:43 -0800

    larger scope for suppression
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- ogre/include/ignition/rendering/ogre/OgreRenderTargetMaterial.hh
+++ ogre/include/ignition/rendering/ogre/OgreRenderTargetMaterial.hh
@@ -24,6 +24,13 @@
 #include "ignition/rendering/ogre/OgreRenderTypes.hh"
 #include "ignition/rendering/ogre/Export.hh"
 
+// Ogre::MaterialManager::Listener isn't a dll-interface class, this may cause
+// issues
+#ifdef _MSC_VER
+ #pragma warning(push)
+ #pragma warning(disable:4275)
+#endif
+
 namespace ignition
 {
   namespace rendering
@@ -44,16 +51,7 @@ namespace ignition
     /// material provided to this class's constructor.
     class IGNITION_RENDERING_OGRE_VISIBLE  OgreRenderTargetMaterial :
       public Ogre::RenderTargetListener,
-// Ogre::MaterialManager::Listener isn't a dll-interface class, this may cause
-// issues
-#ifdef _MSC_VER
- #pragma warning(push)
- #pragma warning(disable:4275)
-#endif
       public Ogre::MaterialManager::Listener
-#ifdef _MSC_VER
- #pragma warning(pop)
-#endif
     {
       /// \brief constructor
       /// \param[in] _scene the scene manager responsible for rendering
@@ -107,5 +105,9 @@ namespace ignition
     }
   }
 }
+
+#ifdef _MSC_VER
+ #pragma warning(pop)
+#endif
 
 #endif

e8c9d0659f016b6f8392bad564957416788a3130
ahcorde <ahcorde@gmail.com>
2021-03-18 14:34:52 +0100

    make linters happy
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- ogre2/include/ignition/rendering/ogre2/Ogre2Capsule.hh
+++ ogre2/include/ignition/rendering/ogre2/Ogre2Capsule.hh
@@ -64,7 +64,8 @@ namespace ignition
         public: virtual MaterialPtr Material() const override;
 
         // Documentation inherited.
-        public: virtual void SetMaterial(MaterialPtr _material, bool _unique) override;
+        public: virtual void
+          SetMaterial(MaterialPtr _material, bool _unique) override;
 
         /// \brief Set material to capsule geometry.
         /// \param[in] _material Ogre material.

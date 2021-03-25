f09108efe0d37c421be8645f10a34800a8e70309
WilliamLewww <WilliamMilesLew@gmail.com>
2021-03-25 15:36:11 -0700

    Added example for custom shaders with different uniform types
    Signed-off-by: William Lew <WilliamMilesLew@gmail.com>

---
+++ examples/custom_shaders_uniforms/media/fragment_shader.glsl
@@ -0,0 +1,11 @@
+uniform float testFloat;
+uniform int testInt;
+uniform float testFloatBuffer[6];
+uniform int testIntBuffer[5];
+uniform vec4 testVector4;
+uniform mat4 textMatrix4;
+
+void main()
+{
+  gl_FragColor = vec4(testFloat, 0.0, 0.0, 1.0);
+}

a255418f644be24899ebab32a9fd8c4b8e496c65
William Lew <WilliamMilesLew@gmail.com>
2021-03-31 16:26:28 -0700

    Added tests for word buffer shader parameter
    Signed-off-by: William Lew <WilliamMilesLew@gmail.com>

--- src/ShaderParam_TEST.cc
+++ src/ShaderParam_TEST.cc
@@ -87,6 +87,58 @@ TEST(ShaderParam, IntType)
   EXPECT_EQ(10, val3);
 }
 
+/////////////////////////////////////////////////
+TEST(ShaderParam, BufferType)
+{
+  ShaderParam p;
+  p.InitializeWordBuffer(12);
+  EXPECT_EQ(ShaderParam::PARAM_WORD_BUFFER, p.Type());
+  EXPECT_EQ(12, (int)p.Count());
+
+  float b[12] =
+  {
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+  };
+
+  p.UpdateWordBuffer(b, 12);
+  std::shared_ptr<float> wordBuffer;
+  EXPECT_TRUE(p.WordBuffer(&wordBuffer));
+
+  for (int x = 0; x < 12; ++x) {
+    EXPECT_EQ(x, (int)wordBuffer.get()[x]);
+  }
+
+  // test copy assignment
+  ShaderParam p2;
+  p2 = p;
+  EXPECT_EQ(ShaderParam::PARAM_WORD_BUFFER, p2.Type());
+  EXPECT_EQ(12, (int)p2.Count());
+
+  std::shared_ptr<float> wordBuffer2;
+  EXPECT_TRUE(p2.WordBuffer(&wordBuffer2));
+
+  for (int x = 0; x < 12; ++x) {
+    EXPECT_EQ(x, (int)wordBuffer2.get()[x]);
+  }
+
+  // test copy constructor
+  ShaderParam p3(p);
+  EXPECT_EQ(ShaderParam::PARAM_WORD_BUFFER, p3.Type());
+  EXPECT_EQ(12, (int)p3.Count());
+
+  std::shared_ptr<float> wordBuffer3;
+  EXPECT_TRUE(p2.WordBuffer(&wordBuffer3));
+
+  for (int x = 0; x < 12; ++x) {
+    EXPECT_EQ(x, (int)wordBuffer3.get()[x]);
+  }
+
+  ShaderParam p4;
+  p4 = 1;
+  std::shared_ptr<float> wordBuffer4;
+  EXPECT_FALSE(p4.WordBuffer(&wordBuffer4));
+}
+
 //////////////////////////////////////////////////
 int main(int argc, char **argv)
 {

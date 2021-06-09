88f6339151e13c28e0393733abbcebde015d0c6b
darksylinc <dark_sylinc@yahoo.com.ar>
2021-06-08 23:58:21 -0300

    Fix floating point precision bug handling alpha channel (#332) (#333)
    Fixes #332
    Fixes #108
    
    Signed-off-by: Matias N. Goldberg <dark_sylinc@yahoo.com.ar>

--- test/integration/depth_camera.cc
+++ test/integration/depth_camera.cc
@@ -254,6 +254,8 @@ void DepthCameraTest::DepthCameraBoxes(
       unsigned int ma = *mrgba >> 0 & 0xFF;
       EXPECT_EQ(0u, mr);
       EXPECT_EQ(0u, mg);
+      // Note: If it fails here, it may be this problem again:
+      // https://github.com/ignitionrobotics/ign-rendering/issues/332
       EXPECT_GT(mb, 0u);
 
       // Far left and right points should be red (background color)
@@ -453,6 +455,8 @@ void DepthCameraTest::DepthCameraBoxes(
           unsigned int a = *rgba >> 0 & 0xFF;
           EXPECT_EQ(0u, r);
           EXPECT_EQ(0u, g);
+          // Note: If it fails here, it may be this problem again:
+          // https://github.com/ignitionrobotics/ign-rendering/issues/332
           EXPECT_GT(b, 0u);
           EXPECT_EQ(255u, a);
         }

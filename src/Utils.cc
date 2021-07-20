9341eb24f37a5c2b1ceb5c2dc07ab8473a2a0f9a
Alejandro Hernández Cordero <ahcorde@gmail.com>
2021-07-21 00:06:39 +0200

    Fixed screento functions names (#368)
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- src/Utils.cc
+++ src/Utils.cc
@@ -35,7 +35,7 @@ namespace rendering
 inline namespace IGNITION_RENDERING_VERSION_NAMESPACE {
 //
 /////////////////////////////////////////////////
-math::Vector3d ScreenToScene(
+math::Vector3d screenToScene(
     const math::Vector2i &_screenPos,
     const CameraPtr &_camera,
     const RayQueryPtr &_rayQuery,
@@ -63,18 +63,18 @@ math::Vector3d ScreenToScene(
 }
 
 /////////////////////////////////////////////////
-math::Vector3d ScreenToScene(
+math::Vector3d screenToScene(
     const math::Vector2i &_screenPos,
     const CameraPtr &_camera,
     const RayQueryPtr &_rayQuery,
     float _maxDistance)
 {
   RayQueryResult rayResult;
-  return ScreenToScene(_screenPos, _camera, _rayQuery, rayResult, _maxDistance);
+  return screenToScene(_screenPos, _camera, _rayQuery, rayResult, _maxDistance);
 }
 
 /////////////////////////////////////////////////
-math::Vector3d ScreenToPlane(
+math::Vector3d screenToPlane(
     const math::Vector2i &_screenPos,
     const CameraPtr &_camera,
     const RayQueryPtr &_rayQuery,

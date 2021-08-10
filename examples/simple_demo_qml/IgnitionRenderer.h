40e34b48ac60e831ce9a0609c73c607f2c0d4d58
Rhys Mainwaring <rhys.mainwaring@me.com>
2021-08-10 14:24:56 +0100

    [macOS] document the IgnitionRenderer class.
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- examples/simple_demo_qml/IgnitionRenderer.h
+++ examples/simple_demo_qml/IgnitionRenderer.h
@@ -22,28 +22,57 @@
 
 #include <ignition/rendering.hh>
 
+/// \brief Ignition renderer class. This manages  the initialisation
+/// and update of an Ignition rendering engine instance and makes the
+/// rendered texture available in a shared context for an application
+/// to apply to a render surface.
 class IgnitionRenderer
 {
 public:
+    /// \brief Destructor
     virtual ~IgnitionRenderer();
+
+    /// \brief Constructor
     IgnitionRenderer();
 
+    /// \brief Render the next frame. May be called on a render thread.
     void Render();
+
+    /// \brief Initialise the render engine and scene. May be called on a render thread.
     void Initialise();
+
+    /// \brief Initialise the render engine and scene. Must be called on the main thread.
     void InitialiseOnMainThread();
 
+    /// \brief Return a boolean: true if the renderer is initialised. 
     bool Initialised() const;
+
+    /// \brief Return the ID of the OpenGL texture.
     unsigned int TextureId() const;
+
+    /// \brief Return the size of the texture
     QSize TextureSize() const;
 
 private:
+    /// \brief Initialise the render engine. Must be called on the main thread. 
     void InitEngine();
+
+    /// \brief Move the camera position one step in its orbit.
     void UpdateCamera();
 
+    /// \brief The OpenGL texture ID
     unsigned int textureId = 0;
+
+    /// \brief The sise of the texture being rendered
     QSize textureSize = QSize(800, 600);
+
+    /// \brief A flag to mark if the renderer has been initialised
     bool initialised = false;
+
+    /// \brief The current camera offset in its orbit
     double cameraOffset = 0.0;
+
+    /// \brief The camera for the example scene
     ignition::rendering::CameraPtr camera;
 };
 

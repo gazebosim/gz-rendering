02fd831b981a9dff115b815d6fa4a97b30b1bb9a
William Lew <WilliamMilesLew@gmail.com>
2021-08-26 08:50:10 -0700

    Reverted simple demo
    Signed-off-by: William Lew <WilliamMilesLew@gmail.com>

--- examples/render_engine/Main.cc
+++ examples/render_engine/Main.cc
@@ -183,7 +183,8 @@ int main(int _argc, char** _argv)
   std::vector<std::string> engineNames;
   std::vector<CameraPtr> cameras;
 
-  engineNames.push_back(ogreEngineName);
+  // engineNames.push_back(ogreEngineName);
+  engineNames.push_back("CustomRenderEngine");
 
   for (auto engineName : engineNames)
   {

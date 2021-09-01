53220ec35889d3b65c9b32c6d9c75917cc0206fe
William Lew <WilliamMilesLew@gmail.com>
2021-09-01 13:57:57 -0700

    Added sample render for luxcore
    Signed-off-by: William Lew <WilliamMilesLew@gmail.com>

---
+++ examples/lux_core_engine/LuxCoreEngineRenderTypes.hh
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#ifndef IGNITION_RENDERING_LUXCOREENGINE_LUXCOREENGINERENDERTYPES_HH_
+#define IGNITION_RENDERING_LUXCOREENGINE_LUXCOREENGINERENDERTYPES_HH_
+
+#include "ignition/rendering/base/BaseRenderTypes.hh"
+
+namespace ignition
+{
+  namespace rendering
+  {
+
+    inline namespace IGNITION_RENDERING_VERSION_NAMESPACE {
+    class LuxCoreEngineCamera;
+    class LuxCoreEngineScene;
+    class LuxCoreEngineSensor;
+    class LuxCoreEngineNode;
+    class LuxCoreEngineObject;
+    class LuxCoreEngineMaterial;
+    class LuxCoreEngineRenderTarget;
+
+    typedef shared_ptr<LuxCoreEngineCamera>               LuxCoreEngineCameraPtr;
+    typedef shared_ptr<LuxCoreEngineScene>                LuxCoreEngineScenePtr;
+    typedef BaseSceneStore<LuxCoreEngineScene>            LuxCoreEngineSceneStore;
+    typedef shared_ptr<LuxCoreEngineSceneStore>           LuxCoreEngineSceneStorePtr;
+    typedef shared_ptr<LuxCoreEngineSensor>               LuxCoreEngineSensorPtr;
+    typedef shared_ptr<LuxCoreEngineNode>                 LuxCoreEngineNodePtr;
+    typedef shared_ptr<LuxCoreEngineObject>               LuxCoreEngineObjectPtr;
+    typedef shared_ptr<LuxCoreEngineMaterial>             LuxCoreEngineMaterialPtr;
+    typedef BaseMaterialMap<LuxCoreEngineMaterial>        LuxCoreEngineMaterialMap;
+    typedef shared_ptr<LuxCoreEngineMaterialMap>          LuxCoreEngineMaterialMapPtr;
+    typedef BaseSensorStore<LuxCoreEngineSensor>          LuxCoreEngineSensorStore;
+    typedef shared_ptr<LuxCoreEngineSensorStore>          LuxCoreEngineSensorStorePtr;
+    typedef shared_ptr<LuxCoreEngineRenderTarget>         LuxCoreEngineRenderTargetPtr;
+
+    }
+  }
+}
+#endif

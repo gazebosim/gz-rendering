b93363622db3fe7827516e47b6bb7b45b182ba62
William Lew <WilliamMilesLew@gmail.com>
2021-09-20 13:35:08 -0700

    Added directional light
    Signed-off-by: William Lew <WilliamMilesLew@gmail.com>

--- examples/lux_core_engine/LuxCoreEngineLight.cc
+++ examples/lux_core_engine/LuxCoreEngineLight.cc
@@ -1,9 +1,23 @@
 #include "LuxCoreEngineLight.hh"
 
+#include "LuxCoreEngineScene.hh"
+
 using namespace ignition;
 using namespace rendering;
 
 ignition::math::Color LuxCoreEngineLight::DiffuseColor() const {}
+
+void LuxCoreEngineLight::SetDiffuseColor(double _r, double _g, double _b, double _a)
+{
+  this->gainR = _r;
+  this->gainG = _g;
+  this->gainB = _b;
+
+  scene->SceneLux()->Parse(
+	    luxrays::Property("scene.lights." + Name() + ".type")(this->lightType) <<
+	    luxrays::Property("scene.lights." + Name() + ".gain")(this->gainR, this->gainG, this->gainB) <<
+      luxrays::Property("scene.lights." + Name() + ".dir")(this->directionX, this->directionY, this->directionZ));
+}
 
 void LuxCoreEngineLight::SetDiffuseColor(const ignition::math::Color &_color) {}
 
@@ -35,6 +49,23 @@ double LuxCoreEngineLight::Intensity() const {}
 
 void LuxCoreEngineLight::SetIntensity(double _intensity) {}
 
+LuxCoreEngineDirectionalLight::LuxCoreEngineDirectionalLight(std::string lightType)
+{
+  this->lightType = lightType;
+}
+
 math::Vector3d LuxCoreEngineDirectionalLight::Direction() const {}
+
+void LuxCoreEngineDirectionalLight::SetDirection(double _x, double _y, double _z)
+{
+  this->directionX = _x;
+  this->directionY = _y;
+  this->directionZ = -_z;
+
+  scene->SceneLux()->Parse(
+	    luxrays::Property("scene.lights." + Name() + ".type")(this->lightType) <<
+	    luxrays::Property("scene.lights." + Name() + ".gain")(this->gainR, this->gainG, this->gainB) <<
+      luxrays::Property("scene.lights." + Name() + ".dir")(this->directionX, this->directionY, this->directionZ));
+}
 
 void LuxCoreEngineDirectionalLight::SetDirection(const math::Vector3d &_dir) {}

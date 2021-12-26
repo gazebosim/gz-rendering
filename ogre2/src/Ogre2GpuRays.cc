gazebosim/gz-rendering

commit 993bb0bff6792627fe1f0cc19108d7280f369da1

Check if key exists in gpu lidar's user data (#396)
Signed-off-by: Ian Chen <ichen@osrfoundation.org>

Co-authored-by: Ashton Larkin <42042756+adlarkin@users.noreply.github.com>

ogre2/src/Ogre2GpuRays.cc
@@ -240,58 +240,58 @@ void Ogre2LaserRetroMaterialSwitcher::cameraPreRenderScene(
       Ogre2VisualPtr ogreVisual =
           std::dynamic_pointer_cast<Ogre2Visual>(result);
 
-      // get laser_retro
-      Variant tempLaserRetro = ogreVisual->UserData(laserRetroKey);
+      if (ogreVisual->HasUserData(laserRetroKey))
+      {
+        // get laser_retro
+        Variant tempLaserRetro = ogreVisual->UserData(laserRetroKey);
 
-      float retroValue = -1.0;
-      try
-      {
-        retroValue = std::get<float>(tempLaserRetro);
-      }
-      catch(...)
-      {
+        float retroValue = -1.0;
         try
         {
-          retroValue = std::get<double>(tempLaserRetro);
+          retroValue = std::get<float>(tempLaserRetro);
         }
         catch(...)
         {
           try
           {
-            retroValue = std::get<int>(tempLaserRetro);
+            retroValue = std::get<double>(tempLaserRetro);
           }
-          catch(std::bad_variant_access &e)
+          catch(...)
           {
-            ignerr << "Error casting user data: " << e.what() << "\n";
-            retroValue = -1.0;
+            try
+            {
+              retroValue = std::get<int>(tempLaserRetro);
+            }
+            catch(std::bad_variant_access &e)
+            {
+              ignerr << "Error casting user data: " << e.what() << "\n";
+              retroValue = -1.0;
+            }
           }
         }
-      }
 
-      // only accept positive laser retro value
-      if (retroValue >= 0)
-      {
-        // set visibility flag so the camera can see it
-        item->addVisibilityFlags(0x01000000);
-        for (unsigned int i = 0; i < item->getNumSubItems(); ++i)
+        // only accept positive laser retro value
+        if (retroValue >= 0)
         {
-          Ogre::SubItem *subItem = item->getSubItem(i);
-          if (!subItem->hasCustomParameter(this->customParamIdx))
+          for (unsigned int i = 0; i < item->getNumSubItems(); ++i)
           {
-            // limit laser retro value to 2000 (as in gazebo)
-            if (retroValue > 2000.0)
+            Ogre::SubItem *subItem = item->getSubItem(i);
+            if (!subItem->hasCustomParameter(this->customParamIdx))
             {
-              retroValue = 2000.0;
+              // limit laser retro value to 2000 (as in gazebo)
+              if (retroValue > 2000.0)
+              {
+                retroValue = 2000.0;
+              }
+              float color = retroValue / 2000.0;
+              subItem->setCustomParameter(this->customParamIdx,
+                  Ogre::Vector4(color, color, color, 1.0));
             }
-            float color = retroValue / 2000.0;
+            Ogre::HlmsDatablock *datablock = subItem->getDatablock();
+            this->datablockMap[subItem] = datablock;
 
-            subItem->setCustomParameter(this->customParamIdx,
-                Ogre::Vector4(color, color, color, 1.0));
+            subItem->setMaterial(this->laserRetroSourceMaterial);
           }
-          Ogre::HlmsDatablock *datablock = subItem->getDatablock();
-          this->datablockMap[subItem] = datablock;
-
-          subItem->setMaterial(this->laserRetroSourceMaterial);
         }
       }
     }
@@ -879,7 +879,17 @@ void Ogre2GpuRays::Setup1stPass()
       passScene->setAllLoadActions(Ogre::LoadAction::Clear);
       passScene->setAllClearColours(Ogre::ColourValue(0, 0, 0));
       // set camera custom visibility mask when rendering laser retro
-      passScene->mVisibilityMask = 0x01000000 &
+      // todo(anyone) currently in this color + depth pass, lidar sees all
+      // objects, including ones without laser_retro set. So the lidar outputs
+      // data containing depth data + some non-zero retro value for all
+      // objects. If we want to output a retro value of zero for objects
+      // without laser_retro, one possible approach could be to separate out
+      // the color and depth pass:
+      // 1: color pass that see only objects with laser_retro by using custom
+      //    visibility mask
+      // 2: depth pass that see all objects
+      // Then assemble these data in the final quad pass.
+      passScene->mVisibilityMask = IGN_VISIBILITY_ALL &
           ~Ogre2ParticleEmitter::kParticleVisibilityFlags;
     }
 
@@ -929,7 +939,7 @@ void Ogre2GpuRays::Setup1stPass()
           particleTargetDef->addPass(Ogre::PASS_SCENE));
       passScene->setAllLoadActions(Ogre::LoadAction::Clear);
       passScene->setAllClearColours(Ogre::ColourValue::Black);
-      // set camera custom visibility mask when rendering laser retro
+      // set camera custom visibility mask when rendering particles
       passScene->mVisibilityMask =
           Ogre2ParticleEmitter::kParticleVisibilityFlags;
     }
@@ -1197,7 +1207,7 @@ void Ogre2GpuRays::CreateGpuRaysTextures()
 /////////////////////////////////////////////////
 void Ogre2GpuRays::UpdateRenderTarget1stPass()
 {
-  Ogre::vector<Ogre::TextureGpu*>::type swappedTargets;
+  Ogre::vector<Ogre::TextureGpu *>::type swappedTargets;
   swappedTargets.reserve(2u);
 
   // update the compositors
@@ -1226,7 +1236,7 @@ void Ogre2GpuRays::UpdateRenderTarget2ndPass()
   this->dataPtr->ogreCompositorWorkspace2nd->_update();
   this->dataPtr->ogreCompositorWorkspace2nd->_endUpdate(false);
 
-  Ogre::vector<Ogre::TextureGpu*>::type swappedTargets;
+  Ogre::vector<Ogre::TextureGpu *>::type swappedTargets;
   swappedTargets.reserve(2u);
   this->dataPtr->ogreCompositorWorkspace2nd->_swapFinalTarget(swappedTargets);
 }

include/ignition/rendering/Node.hh
@@ -332,6 +332,11 @@ namespace ignition
       /// \return Value in any type. If _key does not exist for the node, an
       /// empty variant is returned (i.e., no data).
       public: virtual Variant UserData(const std::string &_key) const = 0;
+
+      /// \brief Check if node has custom data
+      /// \param[in] _key Unique key
+      /// \return True if node has custom data with the specified key
+      public: virtual bool HasUserData(const std::string &_key) const = 0;
     };
     }
   }

include/ignition/rendering/base/BaseNode.hh
@@ -180,6 +180,9 @@ namespace ignition
       // Documentation inherited
       public: virtual Variant UserData(const std::string &_key) const override;
 
+      // Documentation inherited
+      public: virtual bool HasUserData(const std::string &_key) const override;
+
       protected: virtual void PreRenderChildren();
 
       protected: virtual math::Pose3d RawLocalPose() const = 0;
@@ -666,12 +669,14 @@ namespace ignition
     }
     }
 
+    //////////////////////////////////////////////////
     template <class T>
     void BaseNode<T>::SetUserData(const std::string &_key, Variant _value)
     {
      this->userData[_key] = _value;
     }
 
+    //////////////////////////////////////////////////
     template <class T>
     Variant BaseNode<T>::UserData(const std::string &_key) const
     {
@@ -680,6 +685,13 @@ namespace ignition
      if (it != this->userData.end())
        value = it->second;
      return value;
+    }
+
+    //////////////////////////////////////////////////
+    template <class T>
+    bool BaseNode<T>::HasUserData(const std::string &_key) const
+    {
+      return this->userData.find(_key) != this->userData.end();
     }
   }
 }

src/Visual_TEST.cc
@@ -325,42 +325,54 @@ void VisualTest::UserData(const std::string &_renderEngine)
   // int
   std::string intKey = "int";
   int intValue = 1998;
+  EXPECT_FALSE(visual->HasUserData(intKey));
   visual->SetUserData(intKey, intValue);
+  EXPECT_TRUE(visual->HasUserData(intKey));
   Variant value = visual->UserData(intKey);
   EXPECT_EQ(intValue, std::get<int>(value));
 
   // float
   std::string floatKey = "float";
   float floatValue = 1.345f;
+  EXPECT_FALSE(visual->HasUserData(floatKey));
   visual->SetUserData(floatKey, floatValue);
+  EXPECT_TRUE(visual->HasUserData(floatKey));
   value = visual->UserData(floatKey);
   EXPECT_FLOAT_EQ(floatValue, std::get<float>(value));
 
   // double
   std::string doubleKey = "double";
   double doubleValue = -0.123;
+  EXPECT_FALSE(visual->HasUserData(doubleKey));
   visual->SetUserData(doubleKey, doubleValue);
+  EXPECT_TRUE(visual->HasUserData(doubleKey));
   value = visual->UserData(doubleKey);
   EXPECT_DOUBLE_EQ(doubleValue, std::get<double>(value));
 
   // string
   std::string stringKey = "string";
   std::string stringValue = "test_string";
+  EXPECT_FALSE(visual->HasUserData(stringKey));
   visual->SetUserData(stringKey, stringValue);
+  EXPECT_TRUE(visual->HasUserData(stringKey));
   value = visual->UserData(stringKey);
   EXPECT_EQ(stringValue, std::get<std::string>(value));
 
   // bool
   std::string boolKey = "bool";
   bool boolValue = true;
+  EXPECT_FALSE(visual->HasUserData(boolKey));
   visual->SetUserData(boolKey, boolValue);
+  EXPECT_TRUE(visual->HasUserData(boolKey));
   value = visual->UserData(boolKey);
   EXPECT_EQ(boolValue, std::get<bool>(value));
 
   // unsigned int
   std::string unsignedIntKey = "unsignedInt";
   unsigned int unsignedIntValue = 5u;
+  EXPECT_FALSE(visual->HasUserData(unsignedIntKey));
   visual->SetUserData(unsignedIntKey, unsignedIntValue);
+  EXPECT_TRUE(visual->HasUserData(unsignedIntKey));
   value = visual->UserData(unsignedIntKey);
   EXPECT_EQ(unsignedIntValue, std::get<unsigned int>(value));
 
@@ -372,6 +384,7 @@ void VisualTest::UserData(const std::string &_renderEngine)
   EXPECT_FALSE(std::holds_alternative<std::string>(value));
   EXPECT_FALSE(std::holds_alternative<bool>(value));
   EXPECT_FALSE(std::holds_alternative<unsigned int>(value));
+  EXPECT_EQ(0u, value.index());
 
   // test invalid access
   EXPECT_THROW(

test/integration/gpu_rays.cc
@@ -236,7 +236,7 @@ void GpuRaysTest::RaysUnitBox(const std::string &_renderEngine)
   root->AddChild(gpuRays2);
 
   // Laser retro test values
-  double laserRetro1 = 2000;
+  double laserRetro1 = 1500;
   double laserRetro2 = 1000;
   std::string userDataKey = "laser_retro";
 
@@ -250,8 +250,6 @@ void GpuRaysTest::RaysUnitBox(const std::string &_renderEngine)
   visualBox1->SetWorldRotation(box01Pose.Rot());
   visualBox1->SetUserData(userDataKey, laserRetro1);
   root->AddChild(visualBox1);
-
-
 
   // box on the right of the first gpu rays caster
   ignition::math::Pose3d box02Pose(ignition::math::Vector3d(0, -5, 0.5),

commit 5b2a083360076da38af5f00ed6fa04e2ce3c63ac

Fix various issues with Ogre2GpuRays
There were multiple bugs that had to be fixed:

- Objects which had no laser_retro set (or an invalid one) would be
rendered using regular lighting PBS shaders; causing the retro value to
contain garbage instead of the intended default value of 0
- Performance regression in 993bb0bff6792627fe1f0cc19108d7280f369da1:
When ported to Ogre 2.2, ignition would render the scene 4 times instead
of 2 (twice for objects, twice for particles). Depth buffer data was
created through a 2nd redundant pass. This is inefficient as the depth
buffer data is already available from the 1st pass
- Bug regression in 993bb0bff6792627fe1f0cc19108d7280f369da1: In the
pass for normal objects, ignition would first use IGN_VISIBILITY_ALL
flags for colour, but it would use 0x01000000 for depth. This creates
the wrong mix of data because an occluder that had visibility set to
0x01000000 would use the correct depth but the retro value of the object
behind it. This bug is implicitly fixed by the previous item (colour and
depth data are now obtained from the same pass instead of using multi
pass)
- PFG_R8_UNORM is nowhere enough to contain all possible laser retro
values and would quantize too much. Increased precision to PFG_R16_UNORM
- Affects #521. The problem is fixed for Fortress by always overriding
setCustomParameter, but likely the fix should be backported to previous
versions.

Signed-off-by: Matias N. Goldberg <dark_sylinc@yahoo.com.ar>

ogre2/src/Ogre2GpuRays.cc
@@ -238,6 +238,9 @@ void Ogre2LaserRetroMaterialSwitcher::cameraPreRenderScene(
     Ogre::Item *item = static_cast<Ogre::Item *>(object);
 
     std::string laserRetroKey = "laser_retro";
+
+    float retroValue = 0.0f;
+
     // get visual
     Ogre::Any userAny = item->getUserObjectBindings().getUserAny();
     if (!userAny.isEmpty() && userAny.getType() == typeid(unsigned int))
@@ -259,7 +262,6 @@ void Ogre2LaserRetroMaterialSwitcher::cameraPreRenderScene(
         // get laser_retro
         Variant tempLaserRetro = ogreVisual->UserData(laserRetroKey);
 
-        float retroValue = -1.0;
         try
         {
           retroValue = std::get<float>(tempLaserRetro);
@@ -268,7 +270,7 @@ void Ogre2LaserRetroMaterialSwitcher::cameraPreRenderScene(
         {
           try
           {
-            retroValue = std::get<double>(tempLaserRetro);
+            retroValue = static_cast<float>(std::get<double>(tempLaserRetro));
           }
           catch(...)
           {
@@ -279,35 +281,32 @@ void Ogre2LaserRetroMaterialSwitcher::cameraPreRenderScene(
             catch(std::bad_variant_access &e)
             {
               ignerr << "Error casting user data: " << e.what() << "\n";
-              retroValue = -1.0;
             }
-          }
-        }
-
-        // only accept positive laser retro value
-        if (retroValue >= 0)
-        {
-          for (unsigned int i = 0; i < item->getNumSubItems(); ++i)
-          {
-            Ogre::SubItem *subItem = item->getSubItem(i);
-            if (!subItem->hasCustomParameter(this->customParamIdx))
-            {
-              // limit laser retro value to 2000 (as in gazebo)
-              if (retroValue > 2000.0)
-              {
-                retroValue = 2000.0;
-              }
-              float color = retroValue / 2000.0;
-              subItem->setCustomParameter(this->customParamIdx,
-                  Ogre::Vector4(color, color, color, 1.0));
-            }
-            Ogre::HlmsDatablock *datablock = subItem->getDatablock();
-            this->datablockMap[subItem] = datablock;
-
-            subItem->setMaterial(this->laserRetroSourceMaterial);
           }
         }
       }
+
+      // only accept positive laser retro value
+      retroValue = std::max(retroValue, 0.0f);
+    }
+
+    for (unsigned int i = 0; i < item->getNumSubItems(); ++i)
+    {
+      Ogre::SubItem *subItem = item->getSubItem(i);
+
+      // limit laser retro value to 2000 (as in gazebo)
+      if (retroValue > 2000.0f)
+      {
+        retroValue = 2000.0f;
+      }
+      float color = retroValue / 2000.0f;
+      subItem->setCustomParameter(this->customParamIdx,
+                                  Ogre::Vector4(color, color, color, 1.0));
+
+      Ogre::HlmsDatablock *datablock = subItem->getDatablock();
+      this->datablockMap[subItem] = datablock;
+
+      subItem->setMaterial(this->laserRetroSourceMaterial);
     }
     itor.moveNext();
   }
@@ -832,10 +831,6 @@ void Ogre2GpuRays::Setup1stPass()
     depthTexDef->depthBufferId = Ogre::DepthBuffer::POOL_DEFAULT;
     depthTexDef->depthBufferFormat = Ogre::PFG_UNKNOWN;
 
-    Ogre::RenderTargetViewDef *rtv =
-      nodeDef->addRenderTextureView("depthTexture");
-    rtv->setForTextureDefinition("depthTexture", depthTexDef);
-
     Ogre::TextureDefinitionBase::TextureDefinition *colorTexDef =
         nodeDef->addTextureDefinition("colorTexture");
     colorTexDef->textureType = Ogre::TextureTypes::Type2D;
@@ -844,16 +839,21 @@ void Ogre2GpuRays::Setup1stPass()
     colorTexDef->depthOrSlices = 1;
     colorTexDef->widthFactor = 1;
     colorTexDef->heightFactor = 1;
-    colorTexDef->format = Ogre::PFG_R8_UNORM;
+    // We need at least 16-bit because otherwise 256 values are not enough to
+    // store all retro value range
+    colorTexDef->format = Ogre::PFG_R16_UNORM;
     colorTexDef->fsaa = "0";
     colorTexDef->textureFlags &= ~Ogre::TextureFlags::Uav;
     colorTexDef->depthBufferId = Ogre::DepthBuffer::POOL_DEFAULT;
     colorTexDef->depthBufferFormat = Ogre::PFG_D32_FLOAT;
     colorTexDef->preferDepthTexture = true;
 
-    Ogre::RenderTargetViewDef *rtv2 =
+    // Auto setup the RTV then manually override the depth buffer so
+    // it uses the one we created (and thus we can sample from it later)
+    Ogre::RenderTargetViewDef *rtv =
       nodeDef->addRenderTextureView("colorTexture");
-    rtv2->setForTextureDefinition("colorTexture", colorTexDef);
+    rtv->setForTextureDefinition("colorTexture", colorTexDef);
+    rtv->depthAttachment.textureName = "depthTexture";
 
     Ogre::TextureDefinitionBase::TextureDefinition *particleTexDef =
         nodeDef->addTextureDefinition("particleTexture");
@@ -871,11 +871,6 @@ void Ogre2GpuRays::Setup1stPass()
     particleTexDef->depthBufferFormat = Ogre::PFG_D32_FLOAT;
     particleTexDef->preferDepthTexture = true;
 
-    Ogre::RenderTargetViewDef *rtvParticleTexture =
-      nodeDef->addRenderTextureView("particleTexture");
-    rtvParticleTexture->setForTextureDefinition(
-      "particleTexture", particleTexDef);
-
     Ogre::TextureDefinitionBase::TextureDefinition *particleDepthTexDef =
         nodeDef->addTextureDefinition("particleDepthTexture");
     particleDepthTexDef->textureType = Ogre::TextureTypes::Type2D;
@@ -891,12 +886,15 @@ void Ogre2GpuRays::Setup1stPass()
     particleDepthTexDef->textureFlags &= ~Ogre::TextureFlags::Uav;
     particleDepthTexDef->depthBufferFormat = Ogre::PFG_UNKNOWN;
 
-    Ogre::RenderTargetViewDef *rtvparticleDepthTex =
-      nodeDef->addRenderTextureView("particleDepthTexture");
-    rtvparticleDepthTex->setForTextureDefinition(
-      "particleDepthTexture", particleDepthTexDef);
+    // Auto setup the RTV then manually override the depth buffer so
+    // it uses the one we created (and thus we can sample from it later)
+    Ogre::RenderTargetViewDef *rtvParticleTexture =
+      nodeDef->addRenderTextureView("particleTexture");
+    rtvParticleTexture->setForTextureDefinition("particleTexture",
+                                                particleTexDef);
+    rtvParticleTexture->depthAttachment.textureName = "particleDepthTexture";
 
-    nodeDef->setNumTargetPass(5);
+    nodeDef->setNumTargetPass(3);
 
     Ogre::CompositorTargetDef *colorTargetDef =
         nodeDef->addTargetPass("colorTexture");
@@ -921,42 +919,6 @@ void Ogre2GpuRays::Setup1stPass()
       // Then assemble these data in the final quad pass.
       passScene->mVisibilityMask = IGN_VISIBILITY_ALL &
           ~Ogre2ParticleEmitter::kParticleVisibilityFlags;
-    }
-
-    Ogre::CompositorTargetDef *depthTargetDef =
-        nodeDef->addTargetPass("depthTexture");
-    depthTargetDef->setNumPasses(1);
-    {
-      // scene pass
-      Ogre::CompositorPassSceneDef *passScene =
-          static_cast<Ogre::CompositorPassSceneDef *>(
-          depthTargetDef->addPass(Ogre::PASS_SCENE));
-      passScene->setAllLoadActions(Ogre::LoadAction::Clear);
-      passScene->setAllClearColours(Ogre::ColourValue(
-                                      this->FarClipPlane(),
-                                      this->FarClipPlane(),
-                                      this->FarClipPlane()));
-      // depth texture does not contain particles
-      passScene->mVisibilityMask = 0x01000000 &
-          ~Ogre2ParticleEmitter::kParticleVisibilityFlags;
-    }
-
-    Ogre::CompositorTargetDef *particleDepthTargetDef =
-        nodeDef->addTargetPass("particleDepthTexture");
-    particleDepthTargetDef->setNumPasses(1);
-    {
-      // scene pass
-      Ogre::CompositorPassSceneDef *passScene =
-          static_cast<Ogre::CompositorPassSceneDef *>(
-          particleDepthTargetDef->addPass(Ogre::PASS_SCENE));
-      passScene->setAllLoadActions(Ogre::LoadAction::Clear);
-      passScene->setAllClearColours(Ogre::ColourValue(
-                                      this->FarClipPlane(),
-                                      this->FarClipPlane(),
-                                      this->FarClipPlane()));
-      // set camera custom visibility mask when rendering laser retro
-      passScene->mVisibilityMask =
-          Ogre2ParticleEmitter::kParticleVisibilityFlags;
     }
 
     Ogre::CompositorTargetDef *particleTargetDef =
@@ -1074,7 +1036,7 @@ void Ogre2GpuRays::Setup1stPass()
 
     for (auto c : channelsTex)
     {
-      if (c->getPixelFormat() == Ogre::PFG_R8_UNORM)
+      if (c->getPixelFormat() == Ogre::PFG_R16_UNORM)
       {
         // add laser retro material switcher to render target listener
         // so we can switch to use laser retro material when the camera is being
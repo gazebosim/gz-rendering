52a8ad13e6e55082862f08a3764fde93375f7854
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2022-02-06 21:09:37 -0300

    Update VCT data files to 2.3's
    Signed-off-by: Matias N. Goldberg <dark_sylinc@yahoo.com.ar>

--- ogre2/src/media/VCT/ImageVoxelizer/VoxelPartialClear_cs.glsl
+++ ogre2/src/media/VCT/ImageVoxelizer/VoxelPartialClear_cs.glsl
@@ -1,34 +1,32 @@
+// Translates a 3D textures by an UVW offset
+// Matias N. Goldberg Copyright (c) 2021-present
+
 @insertpiece( SetCrossPlatformSettings )
 
-#define OGRE_imageWrite3D4( outImage, iuv, value ) imageStore( outImage, int3( iuv ), value )
+@insertpiece( DeclUavCrossPlatform )
 
-vulkan_layout( ogre_t0 ) uniform texture3D inLightLowerMip;
-vulkan_layout( ogre_t1 ) uniform texture3D inVoxelNormalTex;
+@insertpiece( PreBindingsHeaderCS )
 
 layout( vulkan( ogre_u0 ) vk_comma @insertpiece(uav0_pf_type) )
-uniform restrict writeonly image3D outLightHigherMip0;
-
+uniform restrict image3D dstTex0;
 layout( vulkan( ogre_u1 ) vk_comma @insertpiece(uav1_pf_type) )
-uniform restrict writeonly image3D outLightHigherMip1;
-
+uniform restrict image3D dstTex1;
 layout( vulkan( ogre_u2 ) vk_comma @insertpiece(uav2_pf_type) )
-uniform restrict writeonly image3D outLightHigherMip2;
+uniform restrict image3D dstTex2;
 
 layout( local_size_x = @value( threads_per_group_x ),
 		local_size_y = @value( threads_per_group_y ),
 		local_size_z = @value( threads_per_group_z ) ) in;
 
-//layout( local_size_x = 4,
-//		local_size_y = 4,
-//		local_size_z = 4 ) in;
-
 @insertpiece( HeaderCS )
 
 vulkan( layout( ogre_P0 ) uniform Params { )
-	uniform int higherMipHalfWidth;
+	uniform uint3 startOffset;
+	uniform uint3 pixelsToClear;
 vulkan( }; )
 
-#define p_higherMipHalfWidth higherMipHalfWidth
+#define p_startOffset startOffset
+#define p_pixelsToClear pixelsToClear
 
 //in uvec3 gl_NumWorkGroups;
 //in uvec3 gl_WorkGroupID;
--- ogre2/src/media/VCT/LightVctBounceInject_cs.glsl
+++ ogre2/src/media/VCT/LightVctBounceInject_cs.glsl
@@ -5,32 +5,58 @@
 
 @insertpiece( PreBindingsHeaderCS )
 
-uniform sampler3D voxelAlbedoTex;
-uniform sampler3D voxelNormalTex;
-uniform sampler3D vctProbe;
+@pset( vctTexUnit, 2 )
+
+/// Enable uses_array_bindings when there is more than one cascade
+/// (hlms_num_vct_cascades can't be 0)
+/// uses_array_bindings = hlms_num_vct_cascades - 1
+@psub( uses_array_bindings, hlms_num_vct_cascades, 1 )
+
+vulkan_layout( ogre_t0 ) uniform texture3D voxelAlbedoTex;
+vulkan_layout( ogre_t1 ) uniform texture3D voxelNormalTex;
+vulkan_layout( ogre_t@value(vctTexUnit) ) uniform texture3D vctProbes[@value( hlms_num_vct_cascades )];
+@add( vctTexUnit, hlms_num_vct_cascades )
 
 @property( vct_anisotropic )
-    uniform sampler3D vctProbeX;
-    uniform sampler3D vctProbeY;
-    uniform sampler3D vctProbeZ;
+	vulkan_layout( ogre_t@value(vctTexUnit) ) uniform texture3D vctProbeX[@value( hlms_num_vct_cascades )];
+	@add( vctTexUnit, hlms_num_vct_cascades )
+
+	vulkan_layout( ogre_t@value(vctTexUnit) ) uniform texture3D vctProbeY[@value( hlms_num_vct_cascades )];
+	@add( vctTexUnit, hlms_num_vct_cascades )
+
+	vulkan_layout( ogre_t@value(vctTexUnit) ) uniform texture3D vctProbeZ[@value( hlms_num_vct_cascades )];
+	@add( vctTexUnit, hlms_num_vct_cascades )
 @end
 
-layout (@insertpiece(uav0_pf_type)) uniform restrict writeonly image3D lightVoxel;
+vulkan( layout( ogre_s2 ) uniform sampler vctProbeSampler );
+
+layout( vulkan( ogre_u0 ) vk_comma @insertpiece(uav0_pf_type) )
+uniform restrict writeonly image3D lightVoxel;
 
 layout( local_size_x = @value( threads_per_group_x ),
         local_size_y = @value( threads_per_group_y ),
         local_size_z = @value( threads_per_group_z ) ) in;
 
-uniform float3 voxelCellSize;
-uniform float3 invVoxelResolution;
-uniform float iterationDampening;
-uniform float2 startBias_invStartBias;
+vulkan( layout( ogre_P0 ) uniform Params { )
+	uniform float3 voxelCellSize;
+	uniform float3 invVoxelResolution;
+	uniform float iterationDampening;
+
+	uniform float4 startBias_invStartBias_cascadeMaxLod[@value( hlms_num_vct_cascades )];
+
+	@property( hlms_num_vct_cascades > 1 )
+		uniform float4 fromPreviousProbeToNext[@value( hlms_num_vct_cascades ) - 1][2];
+	@else
+		// Unused, but declare them to shut up warnings of setting non-existant params
+		uniform float4 fromPreviousProbeToNext[1][2];
+	@end
+vulkan( }; )
 
 #define p_voxelCellSize voxelCellSize
 #define p_invVoxelResolution invVoxelResolution
 #define p_iterationDampening iterationDampening
-#define p_vctStartBias startBias_invStartBias.x
-#define p_vctInvStartBias startBias_invStartBias.y
+#define p_vctStartBias_invStartBias_cascadeMaxLod startBias_invStartBias_cascadeMaxLod
+#define p_vctFromPreviousProbeToNext fromPreviousProbeToNext
 
 @insertpiece( HeaderCS )
 

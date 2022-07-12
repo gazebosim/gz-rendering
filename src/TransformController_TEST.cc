51b93cc0fcf53f77f9239b1fb6a374e04dd617c2
Ian Chen <ichen@osrfoundation.org>
2022-07-11 18:13:59 -0700

    add transform control tests
    Signed-off-by: Ian Chen <ichen@osrfoundation.org>

--- src/TransformController_TEST.cc
+++ src/TransformController_TEST.cc
@@ -128,6 +128,8 @@ void TransformControllerTest::TransformControl(const std::string &_renderEngine)
   EXPECT_EQ(math::Vector3d::UnitZ,
       transformControl.ToAxis(TransformAxis::TA_SCALE_Z));
 
+
+
   // Clean up
   engine->DestroyScene(scene);
   rendering::unloadEngine(engine->Name());
@@ -236,29 +238,65 @@ void TransformControllerTest::LocalSpace(const std::string &_renderEngine)
   transformControl.SetTransformSpace(TransformSpace::TS_LOCAL);
   transformControl.SetActiveAxis(math::Vector3d::UnitZ);
   transformControl.Translate(math::Vector3d(0, 0, 2));
-  EXPECT_EQ(visual->WorldPosition(), math::Vector3d(0, -2, 0));
-  EXPECT_EQ(visual->WorldRotation(), initialRot);
-  EXPECT_EQ(visual->WorldScale(), math::Vector3d::One);
+  transformControl.Update();
+  EXPECT_EQ(math::Vector3d(0, -2, 0), visual->WorldPosition());
+  EXPECT_EQ(initialRot, visual->WorldRotation());
+  EXPECT_EQ(math::Vector3d::One, visual->WorldScale());
+
+  // test translation when snapping is enabled
+  transformControl.SetActiveAxis(math::Vector3d::UnitY);
+  transformControl.SetTransformSpace(TransformSpace::TS_WORLD);
+  transformControl.Translate(math::Vector3d(0, 1, 0), true);
+  transformControl.Update();
+  EXPECT_EQ(math::Vector3d(0, -1, 0), visual->WorldPosition());
+  EXPECT_EQ(initialRot, visual->WorldRotation());
+  EXPECT_EQ(math::Vector3d::One, visual->WorldScale());
 
   // test rotation in local space
   transformControl.SetTransformMode(TransformMode::TM_ROTATION);
   transformControl.SetTransformSpace(TransformSpace::TS_LOCAL);
   transformControl.SetActiveAxis(math::Vector3d::UnitX);
   transformControl.Rotate(math::Quaterniond(IGN_PI, 0, 0));
-  EXPECT_EQ(visual->WorldPosition(), math::Vector3d(0, -2, 0));
-  EXPECT_EQ(visual->WorldRotation(),
-      math::Quaterniond(IGN_PI, 0, 0) * initialRot);
-  EXPECT_EQ(visual->WorldScale(), math::Vector3d::One);
+  transformControl.Update();
+  EXPECT_EQ(math::Vector3d(0, -1, 0), visual->WorldPosition());
+  EXPECT_EQ(math::Quaterniond(IGN_PI, 0, 0) * initialRot,
+      visual->WorldRotation());
+  EXPECT_EQ(math::Vector3d::One, visual->WorldScale());
+
+  // test rotation when snapping is enabled
+  transformControl.SetActiveAxis(math::Vector3d::UnitY);
+  transformControl.SetTransformSpace(TransformSpace::TS_WORLD);
+  transformControl.Rotate(math::Quaterniond(0, IGN_PI, 0), true);
+  transformControl.Update();
+  EXPECT_EQ( math::Vector3d(0, -1, 0), visual->WorldPosition());
+  EXPECT_EQ(math::Quaterniond(0, IGN_PI, 0) * math::Quaterniond(IGN_PI, 0, 0) *
+      initialRot, visual->WorldRotation());
+  EXPECT_EQ(math::Vector3d::One, visual->WorldScale());
 
   // test scaling in local space
   transformControl.SetTransformMode(TransformMode::TM_SCALE);
   transformControl.SetTransformSpace(TransformSpace::TS_LOCAL);
   transformControl.SetActiveAxis(math::Vector3d::UnitY);
   transformControl.Scale(math::Vector3d(1.0, 0.3, 1.0));
-  EXPECT_EQ(visual->WorldPosition(), math::Vector3d(0, -2, 0));
-  EXPECT_EQ(visual->WorldRotation(),
-      math::Quaterniond(IGN_PI, 0, 0) * initialRot);
-  EXPECT_EQ(visual->WorldScale(), math::Vector3d(1.0, 0.3, 1.0));
+  transformControl.Update();
+  EXPECT_EQ(math::Vector3d(0, -1, 0), visual->WorldPosition());
+  EXPECT_EQ(math::Quaterniond(0, IGN_PI, 0) * math::Quaterniond(IGN_PI, 0, 0) *
+      initialRot, visual->WorldRotation());
+
+  auto expectedScale = math::Vector3d(1.0, 0.3, 1.0);
+  EXPECT_EQ(expectedScale, visual->WorldScale());
+
+  // test scaling when snapping is enabled
+  auto newScale = math::Vector3d(2.0, 6.0, 1.2);
+  transformControl.Scale(newScale, true);
+  transformControl.Update();
+  EXPECT_EQ(math::Vector3d(0, -1, 0), visual->WorldPosition());
+  EXPECT_EQ(math::Quaterniond(0, IGN_PI, 0) * math::Quaterniond(IGN_PI, 0, 0) *
+      initialRot, visual->WorldRotation());
+  math::Vector3d snappedScale(std::round(newScale.X() * expectedScale.X()),
+                              std::round(newScale.Y() * expectedScale.Y()),
+                              std::round(newScale.Z() * expectedScale.Z()));
+  EXPECT_EQ(snappedScale, visual->WorldScale());
 
   // Clean up
   engine->DestroyScene(scene);
@@ -288,15 +326,29 @@ void TransformControllerTest::Control2d(const std::string &_renderEngine)
 
   TransformController transformControl;
 
-  // test setting camera
-  transformControl.SetCamera(camera);
-  EXPECT_EQ(camera, transformControl.Camera());
+  // test translation and scale without a node
+  math::Vector2d start0(0.5, 0.5);
+  math::Vector2d end0(0.5, 0.8);
+  EXPECT_EQ(math::Vector3d::Zero,
+      transformControl.TranslationFrom2d(math::Vector3d::UnitZ, start0, end0));
+  EXPECT_EQ(math::Vector3d::Zero,
+      transformControl.ScaleFrom2d(math::Vector3d::UnitY, start0, end0));
 
   // create a dummy node visual node and attach to the controller
   VisualPtr visual = scene->CreateVisual();
   ASSERT_NE(nullptr, visual);
   transformControl.Attach(visual);
   EXPECT_EQ(visual, transformControl.Node());
+
+  // test translation and scale without a camera
+  EXPECT_EQ(math::Vector3d::Zero,
+      transformControl.TranslationFrom2d(math::Vector3d::UnitZ, start0, end0));
+  EXPECT_EQ(math::Vector3d::Zero,
+      transformControl.ScaleFrom2d(math::Vector3d::UnitY, start0, end0));
+
+  // test setting camera
+  transformControl.SetCamera(camera);
+  EXPECT_EQ(camera, transformControl.Camera());
 
   // test translation from 2d
   transformControl.SetTransformMode(TransformMode::TM_TRANSLATION);
@@ -305,12 +357,25 @@ void TransformControllerTest::Control2d(const std::string &_renderEngine)
   transformControl.Start();
   math::Vector2d start(0.5, 0.5);
   math::Vector2d end(0.5, 0.8);
+  // translation in z
   math::Vector3d translation =
       transformControl.TranslationFrom2d(math::Vector3d::UnitZ, start, end);
   transformControl.Stop();
   EXPECT_DOUBLE_EQ(translation.X(), 0);
   EXPECT_DOUBLE_EQ(translation.Y(), 0);
   EXPECT_GT(translation.Z(), 0);
+
+  // translation in y
+  transformControl.SetActiveAxis(math::Vector3d::UnitY);
+  transformControl.Start();
+  math::Vector2d starty(0.5, 0.5);
+  math::Vector2d endy(0.2, 0.5);
+  translation =
+      transformControl.TranslationFrom2d(math::Vector3d::UnitY, starty, endy);
+  transformControl.Stop();
+  EXPECT_DOUBLE_EQ(translation.X(), 0);
+  EXPECT_GT(translation.Y(), 0);
+  EXPECT_DOUBLE_EQ(translation.Z(), 0);
 
   // test rotation from 2d
   transformControl.SetTransformMode(TransformMode::TM_ROTATION);

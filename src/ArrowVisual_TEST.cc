fd08fee5056913f11f6ad4e30bc332df23311a8e
ahcorde <ahcorde@gmail.com>
2022-10-12 21:42:17 +0200

    Improved coverage Arrow and Axis visuals and fixed some warnings
    Signed-off-by: ahcorde <ahcorde@gmail.com>

---
+++ src/ArrowVisual_TEST.cc
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2022 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <gtest/gtest.h>
+#include <string>
+
+#include <ignition/common/Console.hh>
+
+#include "test_config.h"  // NOLINT(build/include)
+
+#include "ignition/rendering/ArrowVisual.hh"
+#include "ignition/rendering/RenderEngine.hh"
+#include "ignition/rendering/RenderingIface.hh"
+#include "ignition/rendering/Scene.hh"
+
+using namespace ignition;
+using namespace rendering;
+
+class ArrowVisualTest : public testing::Test,
+                        public testing::WithParamInterface<const char *>
+{
+  /// \brief Test basic API
+  public: void ArrowVisual(const std::string &_renderEngine);
+
+  /// \brief Test gizmo material
+  public: void Material(const std::string &_renderEngine);
+};
+
+/////////////////////////////////////////////////
+void ArrowVisualTest::ArrowVisual(const std::string &_renderEngine)
+{
+  RenderEngine *engine = rendering::engine(_renderEngine);
+  if (!engine)
+  {
+    igndbg << "Engine '" << _renderEngine
+           << "' is not supported" << std::endl;
+    return;
+  }
+
+  ScenePtr scene = engine->CreateScene("scene");
+
+  // create visual
+  ArrowVisualPtr axis = scene->CreateArrowVisual();
+  EXPECT_NE(nullptr, axis);
+  EXPECT_EQ(2u, axis->ChildCount());
+
+  EXPECT_NE(nullptr, axis->Head());
+  EXPECT_NE(nullptr, axis->Shaft());
+
+  EXPECT_NE(axis->Head(), axis->Shaft());
+
+  // Clean up
+  engine->DestroyScene(scene);
+  rendering::unloadEngine(engine->Name());
+}
+
+/////////////////////////////////////////////////
+TEST_P(ArrowVisualTest, ArrowVisual)
+{
+  ArrowVisual(GetParam());
+}
+
+INSTANTIATE_TEST_CASE_P(Visual, ArrowVisualTest,
+    RENDER_ENGINE_VALUES,
+    ignition::rendering::PrintToStringParam());
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}

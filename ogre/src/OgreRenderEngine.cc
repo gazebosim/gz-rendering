4c5707aa494d02d9ad6f4b66654d2d8d03f00a9d
Michael Carroll <mjcarroll@intrinsic.ai>
2024-04-03 02:03:26 +0000

    Work around conda-forge's CDT version of GL
    Signed-off-by: Michael Carroll <mjcarroll@intrinsic.ai>
    Co-authored-by: Silvo Traversaro <silvio@traversaro.it>

--- ogre/src/OgreRenderEngine.cc
+++ ogre/src/OgreRenderEngine.cc
@@ -19,6 +19,16 @@
 #if !defined(__APPLE__) && !defined(_WIN32)
 # include <X11/Xlib.h>
 # include <X11/Xutil.h>
+
+#if !defined(kronos_intptr_t) || !defined(khronos_intptr_t)
+// Conda-forge's glext.h is old and has a bug.
+// This is a minimally-intrusive fix to correct types.
+// On modern GL distributions, glx.h will override these.
+# include <KHR/khrplatform.h>
+typedef khronos_ssize_t GLsizeiptr;
+typedef khronos_intptr_t GLintptr;
+#endif
+
 # include <GL/glx.h>
 #endif
 

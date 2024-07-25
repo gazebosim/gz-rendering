23ca9bea7df68dee47021ff83c6c45021557407f
Ian Chen <ichen@openrobotics.org>
2024-07-25 18:06:25 +0000

    remove use of deprecated env
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- src/base/BaseScene.cc
+++ src/base/BaseScene.cc
@@ -1655,20 +1655,6 @@ void BaseScene::CreateMaterials()
   material->SetLightingEnabled(true);
 
   const char *env = std::getenv("GZ_RENDERING_RESOURCE_PATH");
-
-  // TODO(CH3): Deprecated. Remove on tock.
-  if (!env)
-  {
-    env = std::getenv("IGN_RENDERING_RESOURCE_PATH");
-
-    if (env)
-    {
-      gzwarn << "Using deprecated environment variable "
-             << "[IGN_RENDERING_RESOURCE_PATH]. Please use "
-             << "[GZ_RENDERING_RESOURCE_PATH] instead." << std::endl;
-    }
-  }
-
   std::string resourcePath = (env) ? std::string(env) :
       gz::rendering::getResourcePath();
 

774326e74a62d6aa1d98e1a25dc1da0c62649737
Ian Chen <ichen@openrobotics.org>
2024-08-09 10:03:57 -0700

    Remove more deprecated env vars (#1031)
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- ogre/src/OgreMaterial.cc
+++ ogre/src/OgreMaterial.cc
@@ -821,19 +821,6 @@ void OgreMaterial::SetDepthMaterial(const double _far,
   // Get shader parameters path
   const char *env = std::getenv("GZ_RENDERING_RESOURCE_PATH");
 
-  // TODO(CH3): Deprecated. Remove on tock.
-  if (!env)
-  {
-    env = std::getenv("IGN_RENDERING_RESOURCE_PATH");
-
-    if (env)
-    {
-      gzwarn << "Using deprecated environment variable "
-             << "[IGN_RENDERING_RESOURCE_PATH]. Please use "
-             << "[GZ_RENDERING_RESOURCE_PATH] instead." << std::endl;
-    }
-  }
-
   std::string resourcePath = (env) ? std::string(env) :
       gz::rendering::getResourcePath();
 

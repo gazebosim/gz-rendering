414735f8353d3ab8f09b84bf7bfce8c98472b3cb
Shameek Ganguly <shameek@intrinsic.ai>
2025-04-08 16:23:49 -0700

    Remove unused variable in OgreRenderingEngine.cc (#1119)
    Signed-off-by: Shameek Ganguly <shameek@intrinsic.ai>

--- ogre/src/OgreRenderEngine.cc
+++ ogre/src/OgreRenderEngine.cc
@@ -836,8 +836,6 @@ void OgreRenderEngine::InitAttempt()
 
   this->initialized = false;
 
-  Ogre::ColourValue ambient;
-
   /// Create a dummy rendering context.
   /// This will allow gz-rendering to run headless. And it also allows OGRE to
   /// initialize properly
